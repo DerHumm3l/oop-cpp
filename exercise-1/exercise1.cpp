834bf99d7bb56e36fba39ff5d701127574f9f7e8
DerHumm3l <lukas.maibaum@web.de>
2020-05-17 18:35:11 +0200

    changed hello world

--- exercise-1/exercise1.cpp
+++ exercise-1/exercise1.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string &word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "Hello World" << endl;
 }
