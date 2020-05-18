3cf485ac3ed93166d80bf50a511ef7e688a3a434
DerHumm3l <lukas.maibaum@web.de>
2020-05-18 19:15:36 +0200

    added ui for pi

--- exercise-2/pi.cpp
+++ exercise-2/pi.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -26,11 +27,24 @@ double calcPi(double precision)
 
 int main(int argc, char *argv[])
 {
-    double precision = 0.0000001;
+    double precision;
+    string precisionString;
+
+    cout << "Geben Sie die Praezision fuer die Berechnung von Pi ein: ";
+    getline(cin, precisionString);
+
+    precision = stod(precisionString);
+
+    if (precision <= 0)
+    {
+        cout << "Die Praezision muss groesser 0 sein." << endl;
+    }
 
     double pi = calcPi(precision);
 
     double difference = M_PI - pi;
 
-    cout << difference << endl;
+    cout << "Bei der Praezision von " << precision << " betraegt die Differenz "
+         << "zwischen der Konstante Pi " << M_PI << " und den errechneten Wert "
+         << pi << " mithilfe der Leibniz-Reihe " << difference << endl;
 }
