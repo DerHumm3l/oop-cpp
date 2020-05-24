4096bf770f4dc5c3e0034a825124abd768a2b346
DerHumm3l <lukas.maibaum@web.de>
2020-05-25 01:38:06 +0200

    add getInput

--- exercise-2/computerGuessingGame.cpp
+++ exercise-2/computerGuessingGame.cpp
@@ -18,6 +18,16 @@ void askForNumberBetween(int lowerLimit, int upperLimit)
 {
 }
 
+int getInput(string question)
+{
+    string input;
+
+    cout << question;
+    getline(cin, input);
+
+    return stoi(input);
+}
+
 int main(int argc, char *argv[])
 {
 }
