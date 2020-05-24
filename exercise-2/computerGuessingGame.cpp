d7fecdede4ba17896cb2a24967eafe4e7f734a21
DerHumm3l <lukas.maibaum@web.de>
2020-05-25 01:35:26 +0200

    create stub functions

--- exercise-2/computerGuessingGame.cpp
+++ exercise-2/computerGuessingGame.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void askForNumber(int number)
+{
+}
+
+void askForNumberIsGreater(int number)
+{
+}
+
+void askForNumberIsLess(int number)
+{
+}
+
+void askForNumberBetween(int lowerLimit, int upperLimit)
+{
+}
+
 int main(int argc, char *argv[])
 {
 }
