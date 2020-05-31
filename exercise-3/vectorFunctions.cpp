b75c697eafef9334621f22546fe7983165cda540
DerHumm3l <lukas.maibaum@web.de>
2020-05-31 10:41:05 +0200

    added stub for vectorFunctions

---
+++ exercise-3/vectorFunctions.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+double max(const vector<double> &v)
+{
+}
+
+bool arePositive(const vector<double> &v)
+{
+}
+
+double product(const vector<double> &v1, const vector<double> &v2)
+{
+}
+
+vector<double> product(const vector<double> &v, double f)
+{
+}
+
+double norm(const vector<double> &v)
+{
+}
+
+void normalize(const vector<double> &v)
+{
+}
+
+int main(int argc, char *argv[])
+{
+}
