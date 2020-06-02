44b3dc85627d9b39320404d5ca2d41349d0c3c93
DerHumm3l <lukas.maibaum@web.de>
2020-06-02 12:19:11 +0200

    add basic logic structure

--- exercise-3/stringConvert.cpp
+++ exercise-3/stringConvert.cpp
@@ -3,9 +3,46 @@
 
 using namespace std;
 
+bool checkPreNumber(string &s)
+{
+    return true;
+}
+
+int getNumberFromString(string &s)
+{
+    return 1;
+}
+
+bool checkPostNumber(string &s)
+{
+    return true;
+}
+
 int convert(const string &s)
 {
-    return stoi(s);
+    int number;
+    bool preNumberSuccess, postNumberSuccess;
+
+    string sCopy = s;
+
+    if (!checkPreNumber(sCopy))
+    {
+        // Error
+    }
+
+    number = getNumberFromString(sCopy);
+
+    if (number == -1)
+    {
+        // Error
+    }
+
+    if (!checkPostNumber(sCopy))
+    {
+        // Error
+    }
+
+    return number;
 }
 
 int main(int argc, char *argv[])
@@ -16,7 +53,10 @@ int main(int argc, char *argv[])
     getline(cin, input);
 
     cout << endl
-         << "Die eingegbene Zeichenkette wurde in die Zahl " << to_string(convert(input)) << "umgewandelt." << endl;
+         << "Die eingegbene Zeichenkette wurde in die Zahl " << convert(input) << " umgewandelt." << endl;
+
+    cout << "Enter zum Beenden.";
+    getchar();
 
     return 0;
 }
