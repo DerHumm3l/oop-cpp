32f7e1b7103b6af052ac523af093a65e1d8a300b
DerHumm3l <lukas.maibaum@web.de>
2020-06-02 19:14:51 +0200

    add basic structure for bst

---
+++ exercise-4/bst.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct node
+{
+    node *lNode;
+    node *rNode;
+    string key;
+};
+
+void insert(node &root, string key)
+{
+}
+
+void list(node &root)
+{
+}
+
+void free(node &root)
+{
+}
+
+int main(int argc, char *argv[])
+{
+}
