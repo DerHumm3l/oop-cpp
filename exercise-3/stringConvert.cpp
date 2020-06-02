3952a8fdf190107882811ac5ac2040d95d6910ca
DerHumm3l <lukas.maibaum@web.de>
2020-06-02 10:41:26 +0200

    add basic functionality

--- exercise-3/stringConvert.cpp
+++ exercise-3/stringConvert.cpp
@@ -1,7 +1,22 @@
 #include <string>
+#include <iostream>
 
 using namespace std;
 
+int convert(const string &s)
+{
+    return stoi(s);
+}
+
 int main(int argc, char *argv[])
 {
+    string input;
+
+    cout << "Geben Sie eine Zahl ein: ";
+    getline(cin, input);
+
+    cout << endl
+         << "Die eingegbene Zeichenkette wurde in die Zahl " << to_string(convert(input)) << "umgewandelt." << endl;
+
+    return 0;
 }
