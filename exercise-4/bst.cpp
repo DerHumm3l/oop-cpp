2e1f628c81791e5609a128ec0c5a5dadceaea1cb
DerHumm3l <lukas.maibaum@web.de>
2020-06-03 23:25:58 +0200

    add bst insertion

--- exercise-4/bst.cpp
+++ exercise-4/bst.cpp
@@ -5,13 +5,26 @@ using namespace std;
 
 struct node
 {
-    node *lNode;
-    node *rNode;
+    node *leftChild;
+    node *rightChild;
     string key;
 };
 
-void insert(node &root, string key)
+void insert(node *&root, string key)
 {
+    if (root == nullptr)
+    {
+        root = new node();
+        root->key = key;
+    }
+    else if (root->key.compare(key) > 0)
+    {
+        insert(root->leftChild, key);
+    }
+    else
+    {
+        insert(root->rightChild, key);
+    }
 }
 
 void list(node &root)
@@ -24,4 +37,8 @@ void free(node &root)
 
 int main(int argc, char *argv[])
 {
+    node *root = NULL;
+
+    insert(root, "aaa");
+    insert(root, "bbb");
 }
