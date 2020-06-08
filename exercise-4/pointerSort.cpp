af5e6a58918e02dd620a5d11ec91aa253d4d5e08
DerHumm3l <lukas.maibaum@web.de>
2020-06-08 13:04:37 +0200

    add createPointerList

--- exercise-4/pointerSort.cpp
+++ exercise-4/pointerSort.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 vector<string *> createPointerList(vector<string> &list)
 {
+    vector<string *> pointerList;
+
+    for (string &element : list)
+    {
+        pointerList.push_back(&element);
+    }
+
+    return pointerList;
 }
 
 void sort(vector<string *> &list)
@@ -17,9 +25,10 @@ void print(vector<string *> &list)
 
 int main(int argc, char *argv[])
 {
-    vector<string> list;
+    vector<string> list = {"afds", "adfas", "afgrea", "aasagea"};
 
-    vector<string *> pointerList = createPointerList(list);
+    vector<string *>
+        pointerList = createPointerList(list);
 
     sort(pointerList);
     print(pointerList);
