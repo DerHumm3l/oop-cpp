51c437bf266a6250e09ae7c1bd72d9e02b3a4b0a
DerHumm3l <lukas.maibaum@web.de>
2020-06-09 00:01:04 +0200

    init exercise 5

---
+++ exercise-5/samegame.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ball
+{
+    // color
+}
+
+struct gameBoard
+{
+    int columns,
+        int rows,
+        vector<vector<ball>> balls
+}
+
+int
+main(int argc, char *argv[])
+{
+}
