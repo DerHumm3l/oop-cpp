ba0f2fb2669eb4c3cb7b4d6d88d78f3d4b50735a
DerHumm3l <lukas.maibaum@web.de>
2020-06-09 17:24:34 +0200

    add gameBoard generation

--- exercise-5/samegame.cpp
+++ exercise-5/samegame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ enum color
     yellow = 43,
     blue = 44,
     magenta = 45
-}
+};
 
 struct tile
 {
@@ -23,11 +24,49 @@ struct gameBoard
     vector<vector<tile>> tiles;
 };
 
-int main(int argc, char *argv[])
+void initTiles(vector<tile> &gameTiles)
+{
+    for(tile &gameTile : gameTiles){
+        gameTile.backgroundColor = color(rand() % (magenta - black) + red);
+    }
+}
+
+vector<tile> generateColumn(const int &size){
+    tile gameTile;
+
+    vector<tile> column(size, gameTile);
+
+    initTiles(column);
+
+    return column;
+}
+
+void initColumns(gameBoard &board, const int &rows, const int &columns)
+{
+    for(int i = 0; i < columns; i++){
+        board.tiles.push_back(generateColumn(columns));
+    }
+}
+
+gameBoard generateGameBoard(int rows, int columns)
 {
     gameBoard board;
-    board.tiles = new vector<vector<char>>(9, vector<char>);
 
+    initColumns(board, rows, columns);
+
+    return board;
+}
+
+void printGameBoard(const gameBoard &board)
+{
     cout << "\033[41mHello\033[0m";
+}
+
+int main(int argc, char *argv[])
+{
+    gameBoard board = generateGameBoard(9,9);
+
     getchar();
+
+    return 0;
 }
