aff07e8e7645a82fa421ff73e902df12f35ce1db
DerHumm3l <lukas.maibaum@web.de>
2020-06-09 16:27:12 +0200

    define struct types

--- exercise-5/samegame.cpp
+++ exercise-5/samegame.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
-struct ball
+enum color
 {
-    // color
+    black = 40,
+    red = 41,
+    green = 42,
+    yellow = 43,
+    blue = 44,
+    magenta = 45
 }
+
+struct tile
+{
+    color backgroundColor;
+};
 
 struct gameBoard
 {
-    int columns,
-        int rows,
-        vector<vector<ball>> balls
-}
+    vector<vector<tile>> tiles;
+};
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    gameBoard board;
+    board.tiles = new vector<vector<char>>(9, vector<char>);
+
+    cout << "\033[41mHello\033[0m";
+    getchar();
 }
