f5c77a1b4bfa6ec16988ee8d0f3fd534fd25b887
DerHumm3l <lukas.maibaum@web.de>
2020-06-11 16:49:00 +0200

    add input

--- exercise-5/samegame.cpp
+++ exercise-5/samegame.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
@@ -51,7 +53,6 @@ void initColumns(gameBoard &board)
     for (int i = 0; i < board.numberOfColumns; i++)
     {
         board.tiles.push_back(generateColumn(board.numberOfRows));
-        // board.tiles.push_back(generateColumn(columns));
     }
 }
 
@@ -71,8 +72,21 @@ string getColor(tile gameTile)
     return "\033[" + to_string(gameTile.backgroundColor) + "m \033[0m";
 }
 
-void printRowSeparator(const gameBoard &board)
+void printRowSeparator(const gameBoard &board, bool withHeader = false)
 {
+    if (withHeader)
+    {
+        cout << "   ";
+        for (int i = 0; i < board.numberOfColumns; i++)
+        {
+            cout << char(int('a') + i) << " ";
+        }
+
+        cout << endl;
+    }
+
+    cout << "  ";
+
     for (int i = 0; i < board.numberOfColumns; i++)
     {
         cout << "+-";
@@ -83,9 +97,9 @@ void printRowSeparator(const gameBoard &board)
 
 void printRow(const gameBoard &board, unsigned int rowIndex)
 {
-    for (int j = 0; j < board.numberOfColumns; j++)
+    for (int columnIndex = 0; columnIndex < board.numberOfColumns; columnIndex++)
     {
-        cout << "|" << getColor(board.tiles[j][rowIndex]);
+        cout << "|" << getColor(board.tiles[columnIndex][rowIndex]);
     }
 
     cout << "|" << endl;
@@ -93,26 +107,95 @@ void printRow(const gameBoard &board, unsigned int rowIndex)
 
 void printGameBoard(const gameBoard &board)
 {
-    printRowSeparator(board);
+    printRowSeparator(board, true);
 
-    for (int i = 0; i < board.numberOfRows; i++)
+    for (int rowIndex = 0; rowIndex < board.numberOfRows; rowIndex++)
     {
-        printRow(board, i);
+        cout << rowIndex + 1 << " ";
+
+        printRow(board, rowIndex);
 
         printRowSeparator(board);
     }
 }
 
+int changeGameBoard(gameBoard &board, int columnIndex, int rowIndex)
+{
+    return 0;
+}
+
+bool checkIndices(const gameBoard &board, int columnIndex, int rowIndex)
+{
+    return true;
+}
+
+int applyInput(const string &input, gameBoard &board)
+{
+    if (input.at(0) >= (int)'a' && input.at(0) <= (int)'z')
+    {
+        char columnIndexChar = input.at(0);
+        string rowIndexNumber = input.substr(1);
+
+        if (!rowIndexNumber.empty() && std::find_if(rowIndexNumber.begin(), rowIndexNumber.end(), [](unsigned char c) { return !std::isdigit(c); }) == rowIndexNumber.end())
+        {
+            int columnIndex = (int)'a' - (int)columnIndexChar;
+            int rowIndex = stoi(rowIndexNumber);
+
+            if (checkIndices(board, columnIndex, rowIndex))
+            {
+                changeGameBoard(board, columnIndex, rowIndex);
+                // add Points, etc...
+
+                return 0;
+            }
+            else
+            {
+                return -2; // Not appliable
+            }
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int evaluateInput(const string &input, gameBoard &board)
+{
+    if (!input.empty())
+    {
+        if (input.compare("?") == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return applyInput(input, board);
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    gameBoard board = generateGameBoard(15, 20);
+    string input;
+    gameBoard board = generateGameBoard(9, 9);
 
     printGameBoard(board);
 
-    getchar();
+    cout << endl
+         << "Input: ";
+    getline(cin, input);
 
-    cout << "test 2" << endl;
-    cout << "\033[41mHello\033[0m";
+    int result = evaluateInput(input, board);
+    int b = 0;
 
     getchar();
 
